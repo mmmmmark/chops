8e69b25be925a786a6f0560ffe4c5edc61a0784d
Summer McDonald <summermichiko@gmail.com>
2014-09-09 13:51:32 -0400

    final changes in non-angular app

---
+++ public/javascripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	var likeCounter = 0;
+	$(".like").on("click", function() {
+		$(this).closest(".feedBack").find("img").show();
+	});
+	$(".comment").on("click", function() {
+		
+	});
+});
